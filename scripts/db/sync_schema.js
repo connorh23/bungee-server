fe53f52f7d46d2b72dff217dfef18ebfeeec41e3
Connor Hood <connor.hood234@mail.com>
2021-02-21 21:08:04 -0800

    Create sync_schema.js

---
+++ scripts/db/sync_schema.js
@@ -0,0 +1,11 @@
+const { ModelManager } = require('../../models');
+
+const execute = async () => {
+   await ModelManager.init();
+   await ModelManager.sync();
+   await ModelManager.teardown();
+   console.log("Done");
+};
+
+execute();
+
