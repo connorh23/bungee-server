c5d03e04543f37679ff3aac1135323db187ab373
Connor Hood <connor.hood234@mail.com>
2021-02-23 23:59:31 -0800

    Update env vars to match BUNGEE_ schema

--- environment.js
+++ environment.js
@@ -2,9 +2,9 @@ const { environment } = require('bungee-lib');
 
 module.exports = {
    db: {
-      user: environment.vars.DB_USER,
-      password: environment.vars.DB_PASSWORD,
-      default_db: environment.vars.DEFAULT_DB,
-      host: environment.vars.DB_HOST
+      user: environment.vars.BUNGEE_DB_USER,
+      password: environment.vars.BUNGEE_DB_PASSWORD,
+      default_db: environment.vars.BUNGEE_DEFAULT_DB,
+      host: environment.vars.BUNGEE_DB_HOST
    }
 };
