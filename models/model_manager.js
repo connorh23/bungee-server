5ab59978960e55e722c452bebc4e52bdc9919f8b
Connor Hood <connor.hood234@mail.com>
2021-04-03 23:38:43 -0700

    Update model_manager.js with auto-loaded models from /app directory

--- models/model_manager.js
+++ models/model_manager.js
@@ -2,57 +2,71 @@ let Sequelize = require('sequelize');
 
 const environment = require('../environment');
 
-const {
-	User
-} = require('./app');
+const AppModels = require('./app');
+
 
 module.exports = class ModelManager {
 
-  static orm_client = null;
+	static orm_client;
+	static models;
 
-  static models = {
-  	[User.MODEL_NAME]: User
-  };
+	static get_model = (key) => {
+		return this.models[key];
+	};
 
-  static get_model = (key) => {
-  	return this.models[key];
-  };
+	static init = async () => {
 
-  static init = async () => {
+		return new Promise((success) => {
+			if (!this.models || !this.orm_client) {
+				this.models = {};
+				this.orm_client = new Sequelize(environment.db.default_db, environment.db.user, environment.db.password, {
+					host: environment.db.host,
+					dialect: 'mysql',
+					logging: true
+				});
+				for (const [modelName, modelClass] of Object.entries(AppModels)) {
+					const pathName = this.__generatePathName(modelName);
+					this.models[pathName] = modelClass;
+					modelClass.init(this.orm_client);
+				}
+				success();
+			} else {
+				success();
+			}
+		});
 
-  	return new Promise((success) => {
-  		if (!this.orm_client) {
-  			this.orm_client = new Sequelize(environment.db.default_db, environment.db.user, environment.db.password, {
-  				host: environment.db.host,
-  				dialect: 'mysql',
-  				logging: true
-  			});
-  			Object.values(this.models).forEach(model => {
-  				model.init(this.orm_client);
-  			});
-  			success();
-  		} else {
-  			success();
-  		}
-  	});
+		// this.init_foreign_keys();
+	};
 
-  	// this.init_foreign_keys();
-  };
+	/* istanbul ignore next */
+	static teardown = async () => {
+		await this.orm_client.close();
+	};
 
-  /* istanbul ignore next */
-  static teardown = async () => {
-  	await this.orm_client.close();
-  };
+	static init_foreign_keys = () => {
 
-  static init_foreign_keys = () => {
+		// Beer.hasMany(Category, { foreignKey: 'beer_id' });
+		// Category.belongsTo(Beer, { foreignKey: 'beer_id' });
 
-  	// Beer.hasMany(Category, { foreignKey: 'beer_id' });
-  	// Category.belongsTo(Beer, { foreignKey: 'beer_id' });
+	};
 
-  };
+	static sync = async () => {
+		const models = Object.values(this.models);
+		for (const model of models) {
+			await model.sync({ alter: true });
+		}
+	};
 
-  static sync = async () => {
-  	await User.sync({ alter: true });
-  }
+
+	static __generatePathName = modelName => {
+		let pathName = modelName.charAt(0).toLowerCase() + modelName.slice(1);
+		if (pathName.slice(-1) === "y") {
+			pathName = pathName.substring(0, pathName.length - 1);
+			pathName += "ies"
+		} else {
+			pathName += "s"
+		}
+		return pathName;
+	}
 
 };
