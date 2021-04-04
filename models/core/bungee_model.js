f428d2eaa1dc2aaa0bb04ccbf816d88519354a61
Connor Hood <connor.hood234@mail.com>
2021-04-04 01:12:36 -0700

    Add getUrlPathString to BungeeModel abstract class

--- models/core/bungee_model.js
+++ models/core/bungee_model.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
 class BungeeModel extends Sequelize.Model {
+
    static init = ({ sequelize, modelName, spec, timestamps=true, hooks }) => {
    	super.init(spec, {
    		sequelize,
@@ -8,7 +9,18 @@ class BungeeModel extends Sequelize.Model {
    		timestamps,
    		... hooks && { hooks  }
    	});
-   }
+   };
+
+	static getUrlPathString = ({ modelName }) => {
+		let pathName = modelName.charAt(0).toLowerCase() + modelName.slice(1);
+		if (pathName.slice(-1) === "y") {
+			pathName = pathName.substring(0, pathName.length - 1);
+			pathName += "ies"
+		} else {
+			pathName += "s"
+		}
+		return pathName;
+	}
 }
 
 module.exports = {
