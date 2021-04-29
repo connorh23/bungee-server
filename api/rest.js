260b0988abdf1757aae480569a253a68ba6e74f1
Connor Hood <connor.hood234@mail.com>
2021-04-28 19:10:35 -0700

    Code cleanup

--- api/rest.js
+++ api/rest.js
@@ -1,8 +1,12 @@
-
 const { ModelManager }  = require('../models');
 const { query_utils } = require('../util');
 
-const query = async event => {
+/**
+ *
+ * @param event
+ * @returns {Promise<Model[]>}
+ */
+const query = async ({ event }) => {
 
 	const { model } = event.pathParameters;
 	const query_params = query_utils.get_model_query_params({ model, event });
@@ -16,25 +20,45 @@ const query = async event => {
 	});
 };
 
-const retrieve = async event => {
+/**
+ *
+ * @param event
+ * @returns {Promise<Model|null>}
+ */
+const retrieve = async ({ event }) => {
 	const { id, model } = event.pathParameters;
 	return ModelManager.get_model(model).findByPk(id);
 };
 
-const create = async event => {
+/**
+ *
+ * @param event
+ * @returns {Promise<Model>}
+ */
+const create = async ({ event }) => {
 	const { model } = event.pathParameters;
 	const { body } = event;
 	return ModelManager.get_model(model).create(JSON.parse(body));
 };
 
-const update = async event => {
+/**
+ *
+ * @param event
+ * @returns {Promise<Model>}
+ */
+const update = async ({ event }) => {
 	const { model } = event.pathParameters;
 	const { body } = event;
 	const item = JSON.parse(body);
 	return ModelManager.get_model(model).update(item, { where: { id: item.id } , individualHooks: true });
 };
 
-const destroy = async event => {
+/**
+ *
+ * @param event
+ * @returns {Promise<*>}
+ */
+const destroy = async ({ event }) => {
 	const { id, model } = event.pathParameters;
 	return ModelManager.get_model(model).destroy({ where: { id: id }, individualHooks: true });
 };
