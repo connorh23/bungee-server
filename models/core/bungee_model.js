00492759b43c5dcecb7b441f3d1e8cbc4adb7750
Connor Hood <connor.hood234@mail.com>
2021-04-28 19:12:19 -0700

    Introduce linkClient architecture to model stack

--- models/core/bungee_model.js
+++ models/core/bungee_model.js
@@ -1,16 +1,37 @@
 const Sequelize = require('sequelize');
-
+const ormClient = require('./ormClient');
 class BungeeModel extends Sequelize.Model {
 
-   static init = ({ sequelize, modelName, spec, timestamps=true, hooks }) => {
+	/**
+	 * Initialize the BungeeModel with an ORM connection to the database.
+	 * @param sequelize  - <Sequelize>        Initialized Sequelize ORM
+	 * @param modelName  - <String>           Model Name used for DB table
+	 * @param spec       - <Object>           Model configuration
+	 * @param timestamps - <Boolean>          Collect createdAt & updatedAt?
+	 * @param hooks      - <Object{key:fn()}> Sequelize model lifecycle hooks
+	 */
+   static init = async ({ sequelize, modelName, spec, timestamps=true, hooks }) => {
    	super.init(spec, {
-   		sequelize,
+   		sequelize: await ormClient.getClient(),
    		modelName,
    		timestamps,
    		... hooks && { hooks  }
    	});
    };
 
+   static linkClient = async () => {
+   	await ormClient.getClient();
+	};
+
+	static tearDown() {
+   	ormClient.tearDown();
+	}
+
+	/**
+	 * Get the 'url style' model name.  Example 'User' --> 'users'
+	 * @param modelName
+	 * @returns { string }
+	 */
 	static getUrlPathString = ({ modelName }) => {
 		let pathName = modelName.charAt(0).toLowerCase() + modelName.slice(1);
 		if (pathName.slice(-1) === "y") {
